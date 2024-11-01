662147406a7434fb7f7942bef0d4cb9898af5681
Jack Dong <jackdong986@gmail.com>
2024-11-02 00:24:36 +0800

    updated fine-tuned model in large size

--- fine-tuning/fine-tuned-gpt2.py
+++ fine-tuning/fine-tuned-gpt2.py
@@ -9,7 +9,7 @@ tokenizer.pad_token = tokenizer.eos_token  # Set EOS token as padding
 # Load the dataset and use a subset for training to reduce load
 dataset_path = "recipe_generation_dataset.txt"
 dataset = load_dataset('text', data_files={'train': dataset_path})
-dataset['train'] = dataset['train'].shuffle(seed=42).select(range(1000))  # Adjust the range as needed
+dataset['train'] = dataset['train'].shuffle(seed=42).select(range(10000))  # Adjust the range as needed
 
 # Tokenize the dataset with reduced max_length
 def tokenize_function(examples):
